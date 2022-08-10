af8298be082b96fc501bd0d6891266374af156f4
felixarjuna <felixarjuna.de@yahoo.com>
2022-08-10 18:10:59 +0200

    New Dataset Done

--- src/components/Form/dataset.jsx
+++ src/components/Form/dataset.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
 function Dataset() {
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjA4MzgyMjUsInN1YiI6IjEifQ.stHGynNav-VPyW4ykvuQzWFyd83LiMXFyXAe8Cf43sI";
+
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+
+  const [success, setSuccess] = useState(false);
+
   const [dataset, setDataset] = useState({
     name: "",
     description: "",
-    hours_per_time_step: "",
-    number_of_time_step: "",
-    cost_unit: "",
-    length_unit: "",
+    hours_per_time_step: "1",
+    number_of_time_step: "8760",
+    cost_unit: "€",
+    length_unit: "km",
     ref_created_by: 0,
   });
 
@@ -18,9 +29,35 @@ function Dataset() {
       return { ...prevValue, [name]: value };
     });
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:8080/datasets/", dataset, { headers })
+      .then((res) => {
+        if (res.status === 200) {
+          setSuccess(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    setDataset({
+      name: "",
+      description: "",
+      hours_per_time_step: "1",
+      number_of_time_step: "8760",
+      cost_unit: "€",
+      length_unit: "km",
+      ref_created_by: 0,
+    });
+  }
+
   return (
-    <form className="sidebar-form" action="">
-      <label htmlFor="">
+    <form className="sidebar-form" onSubmit={handleSubmit}>
+      <label htmlFor="name">
         Name
         <input
           name="name"
@@ -29,7 +66,7 @@ function Dataset() {
           type="text"
         />
       </label>
-      <label htmlFor="">
+      <label htmlFor="description">
         Description
         <input
           name="description"
@@ -38,13 +75,22 @@ function Dataset() {
           type="text"
         />
       </label>
-      <label htmlFor="">
+      <label htmlFor="hours_per_time_step">
         Hours per time step
         <input
           name="hours_per_time_step"
           onChange={handleChange}
           type="text"
           value={dataset.hours_per_time_step}
+        />
+      </label>
+      <label htmlFor="number_of_time_step">
+        Number of time step
+        <input
+          name="number_of_time_step"
+          onChange={handleChange}
+          type="text"
+          value={dataset.number_of_time_step}
         />
       </label>
       <label htmlFor="">
@@ -56,7 +102,7 @@ function Dataset() {
           type="text"
         />
       </label>
-      <label htmlFor="">
+      <label htmlFor="length_unit">
         Length unit
         <input
           name="length_unit"
@@ -69,7 +115,14 @@ function Dataset() {
         Upload Zip Archive
         <input name="upload" onChange={handleChange} type="file" />
       </label>
-      <button className="button">+</button>
+      <button className="button" type="submit">
+        +
+      </button>
+      {success ? (
+        <p className="response-success">Dataset successfully created!</p>
+      ) : (
+        ""
+      )}
     </form>
   );
 }
